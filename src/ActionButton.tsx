6a40336ee527ab03e8b116f080ffc4a3ff02dfd0
Prateek Puri <prateekpuri@Prateeks-MacBook-Pro.local>
2024-11-24 18:49:11 +0530

    Added feature to make result uneditable

--- src/ActionButton.tsx
+++ src/ActionButton.tsx
@@ -10,12 +10,12 @@ type ActionButtonProps = {
 export const ActionButton: React.FC<ActionButtonProps> = ({ text }) => {
   const { result, updateResult } = useCalcContext();
   const handleClick = (val: string) => {
-    if (typeof val === "number") {
+    if (typeof result === "string") {
+      updateResult(result + val);
+    } else {
       if (val === "+" || val === "-" || val === "*" || val === "/") {
         updateResult(result + val);
       }
-    } else {
-      updateResult(result + val);
     }
   };
 
